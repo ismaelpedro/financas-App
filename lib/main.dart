50fd23df577ca795cbab70677d79e75359513d66
Ismael Pedro <http.ismaeel@gmail.com>
2021-02-13 12:34:11 -0300

    Criando layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'dart:html';
-
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -14,21 +13,52 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 1,
+      title: 'Novo Tênis de Corrida',
+      value: 310.67,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 2,
+      title: 'Conta de Luz',
+      value: 211.35,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Finanças'),
         centerTitle: true,
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.purple,
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Card(
-            child: Text('Gráfico'),
+          Container(
+            child: Card(
+              child: Text(
+                'Gráfico',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              elevation: 5,
+              color: Colors.purple,
+            ),
           ),
-          Card(
-            child: Text('Lista de transações'),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
           ),
         ],
       ),
